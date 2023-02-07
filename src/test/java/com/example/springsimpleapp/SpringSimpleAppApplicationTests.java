7978617b59ba9f54d967e9d14cfd64db61a0d27d
cmglmsr <mgulumser@my.aci.k12.tr>
2023-02-07 23:14:22 +0300

    Spring initialize

---
+++ src/test/java/com/example/springsimpleapp/SpringSimpleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsimpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
